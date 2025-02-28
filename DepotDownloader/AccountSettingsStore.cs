7609f2090ddce17a2fbf25843049e511e1c325b2
Oreeeee <oree3ee@gmail.com>
2025-02-28 10:00:46 +0100

    Remove the dependency on IsolatedStorage in AccountSettingStore

--- DepotDownloader/AccountSettingsStore.cs
+++ DepotDownloader/AccountSettingsStore.cs
@@ -42,31 +42,14 @@ namespace DepotDownloader
         }
 
         public static AccountSettingsStore Instance;
-        static readonly IsolatedStorageFile IsolatedStorage = IsolatedStorageFile.GetUserStoreForAssembly();
+        //static readonly IsolatedStorageFile IsolatedStorage = IsolatedStorageFile.GetUserStoreForAssembly();
 
         public static void LoadFromFile(string filename)
         {
             if (Loaded)
                 throw new Exception("Config already loaded");
 
-            if (IsolatedStorage.FileExists(filename))
-            {
-                try
-                {
-                    using var fs = IsolatedStorage.OpenFile(filename, FileMode.Open, FileAccess.Read);
-                    using var ds = new DeflateStream(fs, CompressionMode.Decompress);
-                    Instance = Serializer.Deserialize<AccountSettingsStore>(ds);
-                }
-                catch (IOException ex)
-                {
-                    Console.WriteLine("Failed to load account settings: {0}", ex.Message);
-                    Instance = new AccountSettingsStore();
-                }
-            }
-            else
-            {
-                Instance = new AccountSettingsStore();
-            }
+            Instance = new AccountSettingsStore();
 
             Instance.FileName = filename;
         }
@@ -76,16 +59,8 @@ namespace DepotDownloader
             if (!Loaded)
                 throw new Exception("Saved config before loading");
 
-            try
-            {
-                using var fs = IsolatedStorage.OpenFile(Instance.FileName, FileMode.Create, FileAccess.Write);
-                using var ds = new DeflateStream(fs, CompressionMode.Compress);
-                Serializer.Serialize(ds, Instance);
-            }
-            catch (IOException ex)
-            {
-                Console.WriteLine("Failed to save account settings: {0}", ex.Message);
-            }
+            // TODO
+            return;
         }
     }
 }
